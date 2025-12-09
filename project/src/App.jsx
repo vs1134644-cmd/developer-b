45a1a26c001a5e27ce53a41af7e13a0996d8f032
Tumhara Naam <tum@example.com>
2025-12-09 10:57:17 +0530

    notes read

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -20,37 +20,94 @@
 // export default App
 
 
-import React from 'react'
 
+
+
+import { useState } from 'react'
 
 const App = () => {
-  const SubmitHandler = (e) =>{
+
+  const [title, setTitle] = useState('')
+  const [details, setDetails] = useState('')
+
+  const [task, setTask] = useState([])
+
+  const submitHandler = (e) => {
     e.preventDefault()
-    console.log('Form Submitted')
-  
-}
+
+    const copyTask = [...task];
+
+    copyTask.push({ title, details })
+
+    setTask(copyTask)
+
+    setTitle('')
+    setDetails('')
+  }
+
+
+  const deleteNote = (idx) => {
+    const copyTask = [...task];
+
+    copyTask.splice(idx, 1)
+
+    setTask(copyTask)
+  }
+
   return (
-    <div className='h-screen bg-black text-white'>
-      <form
-      onSubmit={(e) =>{
-        SubmitHandler(e)
-      }} 
-      className='flex  gap-4 flex-col justify-between items-start  p-10'>
-        
-        <input type="text"
+    <div className='h-screen lg:flex bg-black text-white'>
+
+      <form onSubmit={(e) => {
+        submitHandler(e)
+      }} className='flex gap-4 lg:w-1/2 p-10 flex-col items-start'>
+
+        <h1 className='text-4xl mb-2 font-bold'>Add Notes</h1>
+
+        {/* PEHLA INPUT FOR HEADING */}
+        <input
+          type="text"
           placeholder='Enter Notes Heading'
-          className=' font-medium  w-full   px-5 py-2  border-2 rounded outline-none '
-          />
+          className='px-5 w-full font-medium py-2 border-2 outline-none rounded '
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value)
+          }}
+        />
+
+        {/* DETAILED VALA INPUT  */}
         <textarea
-         type="text"
-          placeholder='write Details'
-          className='  font-medium px-5 py-2 w-full h-30 flex items-start flex-row border-2 rounded outline-none'
-          />
-        <button className='  font-medium bg-white w-full outline-none text-black px-5 py-2 rounded'>Add Now</button>
-          
+          type="text"
+          className='px-5 w-full font-medium h-32 py-2 flex items-start flex-row border-2 outline-none  rounded '
+          placeholder='Write Details here'
+          value={details}
+          onChange={(e) => {
+            setDetails(e.target.value)
+          }}
+        />
+
+        <button
+          className='bg-white active:scale-95 font-medium w-full outline-none  text-black px-5 py-2 rounded'
+        >
+          Add Note
+        </button>
+
       </form>
-      <div className='flex flex-wrap p-10'>
-        <div className='h-32 w-32 rounded-2xl bg-white'></div>
+      <div className='lg:w-1/2 lg:border-l-2  p-10'>
+        <h1 className='text-4xl font-bold'>Recent Notes</h1>
+        <div className='flex flex-wrap items-start justify-start gap-5 mt-6 h-[90%] overflow-auto'>
+          {task.map(function (elem, idx) {
+
+            return <div key={idx} className=" flex justify-between flex-col items-start relative h-52 w-40 bg-cover rounded-xl text-black pt-9 pb-4 px-4 bg-[url('https://static.vecteezy.com/system/resources/previews/037/152/677/non_2x/sticky-note-paper-background-free-png.png')]">
+              <div>
+                <h3 className='leading-tight text-lg font-bold'>{elem.title}</h3>
+                <p className='mt-2 leading-tight text-xs font-semibold text-gray-600'>{elem.details}</p>
+              </div>
+              <button onClick={() => {
+                deleteNote(idx)
+              }} className='w-full cursor-pointer active:scale-95 bg-red-500 py-1 text-xs rounded font-bold text-white'>Delete</button>
+            </div>
+          })}
+        </div>
       </div>
     </div>
   )

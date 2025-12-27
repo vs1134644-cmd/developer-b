0743dabb4f272e3a990daef41d0dd906f13c78a3
Tumhara Naam <tum@example.com>
2025-12-27 12:48:57 +0530

    dfghjkl

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,23 +1,4 @@
-// import React from 'react'
-// import Page2 from './components/page2/page2'
-// import Page1 from './components/Page1/Page1'
 
-
-
-// const App = () => {
-//   return (
-//     <div>
-
-//       <Page1/>
-//       <Page2/>
-
-
-
-//     </div>
-//   )
-// }
-
-// export default App
 
 
 
